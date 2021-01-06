a440741a3d5739f0ff01726c9d6eb268f2faa97a
sushmasabbani <41639390+sushmasabbani@users.noreply.github.com>
2021-01-06 21:02:34 +0530

    Update main.php

--- main.php
+++ main.php
@@ -1,12 +1,8 @@
-#<?php
+<?php
   #echo "This is the PHP application";
   #$string = env('string');
  # print string;
 #?>
-<html>
-  <head>
-  <h1>CHecking Environment Variables</h1>
   $var1= "Hello World";
   echo printf($var1);
-  </head>
-</html>
+?>
