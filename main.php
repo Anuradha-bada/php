78dd79e675469ed26e062c889ef62d93ab5b2961
sushmasabbani <41639390+sushmasabbani@users.noreply.github.com>
2021-01-07 09:47:32 +0530

    Update main.php

--- main.php
+++ main.php
@@ -5,12 +5,12 @@
 <h1>My first PHP page</h1>
 
 <?php
-   include('config.php');
+   #include('config.php');
    echo "Hello World!";
    echo "<br>";
    $string="Default Varibale";
    echo "<br>;
-   echo $username;
+   #echo $username;
    #echo $string;
 ?> 
 
