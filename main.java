74f2682a9677da75f3cd04b4f87c920dda77aefa
sushmasabbani <41639390+sushmasabbani@users.noreply.github.com>
2021-01-07 21:09:14 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,7 @@
+public static void main
+{
+  Properties prop=new Properties();
+  FileInputStream ip= new FileInputStream(“php/config.properties”);
+  prop.load(ip);
+  out.println(prop.getProperty(“key1”));
+}
