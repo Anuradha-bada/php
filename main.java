10ca0a6c597fa22a678a7577fe2e256423fdc212
sushmasabbani <41639390+sushmasabbani@users.noreply.github.com>
2021-01-07 21:28:37 +0530

    Delete main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-public static void main
-{
-  Properties prop=new Properties();
-  FileInputStream ip= new FileInputStream(“php/config.properties”);
-  prop.load(ip);
-  out.println(prop.getProperty(“key1”));
-  out.println("Hello World");
-}
